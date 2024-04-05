d1c65e5c0173135f8a13fa7ae6da9ab85a7abf99
James <91065484+JamesACS@users.noreply.github.com>
2024-04-05 15:06:22 +0000

    feat: Adding back button to blog posts as header and footer to improve navigation experience

--- app/blog/posts/[...slug]/page.tsx
+++ app/blog/posts/[...slug]/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 import { allPosts } from "contentlayer/generated";
 import { format, parseISO } from "date-fns";
+import { ArrowRightIcon, ChevronLeftSquare } from "lucide-react";
 
 import { Mdx } from "@/mdx-components";
 
@@ -47,53 +48,81 @@ export async function generateStaticParams(): Promise<PostProps["params"][]> {
 
 export default async function PostPage({ params }: PostProps) {
   const post = await getPostFromParams(params);
-  
 
   if (!post) {
     notFound();
   }
 
   return (
-    <article className="prose dark:prose-invert max-h-full max-w-full mb-12">
-      {post.image && (
-        <div className="relative mb-12 h-96 w-full">
-          <Image
-            className="m-0 w-full rounded-lg object-cover"
-            src={post.image}
-            alt={post.title}
-            fill
-            priority
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-      )}
-
-      <header>
-        <h1 className="mb-2">{post.title}</h1>
-        {post.description && (
-          <p className="mb-6 mt-0 text-xl text-gray-700 dark:text-colortext">
-            {post.description}
-          </p>
-        )}
-        <p className="space-x-1 text-xs text-tertiary">
-          <span>{format(parseISO(post.date), "MMMM dd, yyyy")}</span>
-          <span>{` • `}</span>
-          <span>{post.readingTime.text}</span>
-          <span>{` • `}</span>
-          <span>
-            <Link
-              href={`/blog/categories/${encodeURIComponent(
-                post.category.toLowerCase(),
-              )}`}
-              className="text-tertiary underline underline-offset-2 "
-            >
-              {post.category}
-            </Link>
-          </span>
-        </p>
+    <div className="space-y-8">
+      <header className="flex items-center space-x-1">
+        <Link
+          href={`/blog/categories/${encodeURIComponent(
+            post.category.toLowerCase(),
+          )}`}
+        >
+          <ChevronLeftSquare className=" h-6 w-6" />
+        </Link>
+        <h1 className="text-1xl">
+          <span className="font-semibold">Back</span>
+        </h1>
       </header>
-      <hr className="my-6" />
-      <Mdx code={post.body.code} />
-    </article>
+
+      <article className="prose dark:prose-invert max-h-full max-w-full mb-12">
+        {post.image && (
+          <div className="relative mb-12 h-96 w-full">
+            <Image
+              className="m-0 w-full rounded-lg object-cover"
+              src={post.image}
+              alt={post.title}
+              fill
+              priority
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
+          </div>
+        )}
+
+        <header>
+          <h1 className="mb-2">{post.title}</h1>
+          {post.description && (
+            <p className="mb-6 mt-0 text-xl text-gray-700 dark:text-colortext">
+              {post.description}
+            </p>
+          )}
+          <p className="space-x-1 text-xs text-tertiary">
+            <span>{format(parseISO(post.date), "MMMM dd, yyyy")}</span>
+            <span>{` • `}</span>
+            <span>{post.readingTime.text}</span>
+            <span>{` • `}</span>
+            <span>
+              <Link
+                href={`/blog/categories/${encodeURIComponent(
+                  post.category.toLowerCase(),
+                )}`}
+                className="text-tertiary underline underline-offset-2 "
+              >
+                {post.category}
+              </Link>
+            </span>
+          </p>
+        </header>
+        <hr className="my-6" />
+        <Mdx code={post.body.code} />
+      </article>
+      <div className="pb-10">
+        <footer className="flex items-center space-x-1">
+          <Link
+            href={`/blog/categories/${encodeURIComponent(
+              post.category.toLowerCase(),
+            )}`}
+          >
+            <ChevronLeftSquare className=" h-6 w-6" />
+          </Link>
+          <h1 className="text-1xl">
+            <span className="font-semibold">Back</span>
+          </h1>
+        </footer>
+      </div>
+    </div>
   );
 }
